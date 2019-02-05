d290a5ff24163f42cf075361aca42ebe79fbb2c0
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-05 14:21:47 -0800

    Added an auto cylcling interval to the carousel.

--- Components/Carousel/Carousel.js
+++ Components/Carousel/Carousel.js
@@ -9,22 +9,23 @@ class Carousel {
         this.itemList[this.index].style.opacity = '1'
         this.leftBtn.addEventListener("click", () => this.cycleLeft());
         this.rightBtn.addEventListener("click", () => this.cycleRight());
+        setInterval(() => this.cycleRight(), 10000)
     }
     
     cycleLeft () {
         this.itemList.forEach(story => TweenMax.to(story, 2, {opacity: 0, display: 'none'}))
         TweenMax.to(this.itemList[this.index] , 1.5, {x: -1500} )
-        TweenMax.to(this.itemList[this.index] , 0, { delay: .80,  x: 0, display: 'none'} )
+        TweenMax.to(this.itemList[this.index] , 0, { delay: .80,  x: 0, display: 'none'})
         this.index === 0 ? this.index = this.itemList.length-1 : this.index --
-        TweenMax.to(this.itemList[this.index] , 1.5, { display: 'block', delay: .8, opacity :1} )
+        TweenMax.to(this.itemList[this.index] , 1.5, { display: 'block', delay: .8, opacity :1})
     }
     
     cycleRight () {
         this.itemList.forEach(story => TweenMax.to(story, 2, {opacity: 0}))
         TweenMax.to(this.itemList[this.index] , 1.5, {x: 1500} )
-        TweenMax.to(this.itemList[this.index] , 0, { delay: .80, display: 'none', x: 0} )
+        TweenMax.to(this.itemList[this.index] , 0, { delay: .80, display: 'none', x: 0})
         this.index == this.itemList.length - 1 ? this.index = 0 : this.index ++;
-        TweenMax.to(this.itemList[this.index] , 2, { display: 'block', delay: .8, opacity :1} )
+        TweenMax.to(this.itemList[this.index] , 2, { display: 'block', delay: .8, opacity :1})
     }
   }
    
